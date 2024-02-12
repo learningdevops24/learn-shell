d7f5bc232e34529f3593a406c5ac8c3555c993ef
anilk <anilk0001234@gmail.com>
2024-02-12 20:06:06 +0530

    Shell script Program for Variables

--- Variables.sh
+++ Variables.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 ## If we assign a name to set of data is called as Variable.
-trainer=Raghu
-class=DevOps
+TRAINER=Raghu
+CLASS=DevOps
 
-echo -e "\e[35mTrainer Name - $trainer\e[0m"
-echo -e "\e[34mClass Name - ${class}\e[0m"
+echo -e "\e[35mTrainer Name - $TRAINER\e[0m"
+echo -e "\e[34mClass Name - ${CLASS}\e[0m"
 DATE=$(date +%F)
 echo "Hey Today's date is $DATE"
 
